c9c1b85735b0ea2e597eef2a015f695d13d4f73a
YassineSE1 <yassineasseban@gmail.com>
2023-02-17 11:03:26 -0800

    print comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
+
+int main(void)
+{
+int i;
+int j;
+int k;
+int l;
+for (i = 0; i < 10 ; i++)
+{
+for (j = 0; j < 10 ; j++)
+{
+for (k = 0; k < 10 ; k++)
+{
+for (l = 0; l < 10 ; l++)
+{
+if (i == j && k != l && k != j)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(' ');
+putchar(k + '0');
+putchar(l + '0');
+putchar(',');
+putchar(' ');
+}
+else
+putchar('\n');
+}
+}
+}
+}
+return (0);
+}
