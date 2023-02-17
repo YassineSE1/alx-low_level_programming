e5444a14f0e89c6bb17cd5a1555506d04f0bcd4c
YassineSE1 <yassineasseban@gmail.com>
2023-02-17 08:25:38 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,13 @@
 
 int main(void)
 {
-  
+
 char j;
 char i;
-for( i = 'a' ; i <= 'z'; i++)
-putchar('i');
- for ( j = 'A' ; j <= 'Z';j++)
-putchar('j');
+for (i = 'a' ; i <= 'z'; i++)
+putchar(i);
+for (j = 'A' ; j <= 'Z'; j++)
+putchar(j);
 putchar('\n');
 return (0);
 
