2e0c52e9e98e98c0fe8993fc082155a18e0ce18c
YassineSE1 <yassineasseban@gmail.com>
2023-02-17 08:21:08 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
+
+int main(void)
+{
+  
+char j;
+char i;
+for( i = 'a' ; i <= 'z'; i++)
+putchar('i');
+ for ( j = 'A' ; j <= 'Z';j++)
+putchar('j');
+putchar('\n');
+return (0);
+
+}
