4a1f46d09be20acfb1849d93036533157199d945
YassineSE1 <yassineasseban@gmail.com>
2023-02-17 10:43:01 -0800

    print comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 1 (Success) 
+*/
+
+int main(void)
+{
+int i;
+int j;
+int k;
+for (i = 0; i < 10 ; i++)
+{
+for (j = 0; j < 10 ; j++)
+{
+for (k = 0; k < 10 ; k++)
+{
+if (i < j && i != j)
+if (j < k && j != k && i!= k)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
+if (i + j + k != 24)
+{
+putchar(',');
+putchar(' ');
+}
+else
+putchar('\n');
+}
+}
+}
+}
+return (0);
+}
