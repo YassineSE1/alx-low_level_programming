0021f8227bbf07aab39ae1581b9c0f40144821ad
YassineSE1 <yassineasseban@gmail.com>
2023-02-17 02:26:54 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n");
 return (1);
 
 }
