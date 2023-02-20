60a91a7a5072130ec45fea77fab3b719d7ea4e86
YassineSE1 <yassineasseban@gmail.com>
2023-02-20 10:04:02 -0800

    combo 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,7 +20,8 @@ putchar((num1 % 10) + '0');
 putchar(' ');
 putchar((num2 / 10) + '0');
 putchar((num2 % 10) + '0');
-if (num1 != 98)
+if (num1 == 98 && num2 == 99)
+break;
 putchar(',');
 putchar(' ');
 }
