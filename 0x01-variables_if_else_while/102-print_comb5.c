3eba2b9cbea74d4f482a4666284ebb05a80b7e90
YassineSE1 <yassineasseban@gmail.com>
2023-02-20 07:25:19 -0800

    combo 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,33 +8,23 @@
 
 int main(void)
 {
-int i;
-int j;
-int k;
-int l;
-for (i = 0; i < 10 ; i++)
+int num1;
+int num2;
+
+for (num1 = 0; num1 <= 98 ; num1++)
 {
-for (j = 0; j < 10 ; j++)
+for (num2 = num1 + 1; num2 <=99 ; num2++)
 {
-for (k = 0; k < 10 ; k++)
-{
-for (l = 0; l < 10 ; l++)
-{
-if (i + j < k + l)
-{
-putchar(i + '0');
-putchar(j + '0');
+putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
 putchar(' ');
-putchar(k + '0');
-putchar(l + '0');
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
+if (num1 != 98)
 putchar(',');
 putchar(' ');
 }
-else if (i + j == 17 && k + l == 18)
+}
 putchar('\n');
-}
-}
-}
-}
 return (0);
 }
